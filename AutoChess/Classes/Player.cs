22bbeb0af0611b7480411094065857fd41435a36
dmitr <dmitrgares@gmail.com>
2024-12-04 11:25:41 +0700

    Добвлен класс игрок и класс игра

---
+++ AutoChess/Classes/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoChess.Classes
+{
+    public class Player
+    {
+        public string Name { get; }
+        public List<Unit> Units { get; }
+        
+        public Player(string name = "default")
+        {
+            Name = name;
+            Units = new List<Unit>();
+        }
+        public void AddUnit(Unit unit)
+        {
+            Units.Add(unit);
+        }
+        public bool HasAliveUnits() 
+        { 
+            return Units.Any(unit => unit.Health > 0);
+        }
+    }
+}
